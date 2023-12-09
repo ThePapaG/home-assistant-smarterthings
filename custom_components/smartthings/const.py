3cf327d05f7af19e0624573ec659a316a6ee9f51
ThePapaG <gdare1@yahoo.com.au>
2023-12-10 08:23:51 +1000

    Fix domain

--- custom_components/smartthings/const.py
+++ custom_components/smartthings/const.py
@@ -4,7 +4,7 @@ import re
 
 from homeassistant.const import Platform
 
-DOMAIN = "smarterthings"
+DOMAIN = "smartthings"
 
 APP_OAUTH_CLIENT_NAME = "Home Assistant"
 APP_OAUTH_SCOPES = ["r:devices:*"]
